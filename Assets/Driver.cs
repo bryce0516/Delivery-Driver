5c0ce6e764f62c0c8cb94a02d4f0d29dbf285e7c
Hyesung Park <jayhspark@gmail.com>
2024-02-28 11:36:45 +0900

    add speed

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  float steerSpeed = 0.1f;
+  float moveSpeed = 0.01f;
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.Rotate(0, 0, steerSpeed);
+    transform.Translate(0, moveSpeed, 0);
+  }
 }
